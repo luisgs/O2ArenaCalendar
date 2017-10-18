f40279a5268efa0b7e88a9d6f0ec78586d4f7fce
Ubuntu <luisgs@MaquinaUbuntu.mistermapache.f9.internal.cloudapp.net>
2017-10-18 15:52:50 +0200

    using curl and Beutiful Soup to extract our events from O2 Arena website

---
+++ o2calendar.py
@@ -0,0 +1,27 @@
+import sys, logging
+import urllib2
+from bs4 import BeautifulSoup
+
+O2EventsURL = "https://www.o2arena.cz/en/events/"
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
+
+def cleanHTML(O2RawPage):
+    soup = BeautifulSoup(O2RawPage, 'html.parser')
+    print soup.find_all("div", attrs={"id": "events_index"})
+
+
+def curlURL(hola):
+    logging.info('curlURL function')
+    # we request to read an URL
+    page = urllib2.urlopen(O2EventsURL)
+    # we read it and save it
+    page_content = page.read()
+    cleanHTML(page_content)
+
+
+def main():
+    curlURL(O2EventsURL)
+
+if __name__ == "__main__":
+    sys.exit(main())
