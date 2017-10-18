d0937805da08c46a42afa7f18ab31718a8b5c53c
luisgs <luis.ild@gmail.com>
2017-10-18 23:59:32 +0200

    We scrap o2 arena website, retrieve all events info and fullfil our LIst wit this info

--- o2calendar.py
+++ o2calendar.py
@@ -2,29 +2,61 @@ import sys, logging
 import urllib2
 from bs4 import BeautifulSoup
 
+
 O2EventsURL = "https://www.o2arena.cz/en/events/"
+
+
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 
-def cleanHTML(O2RawPage):
-    soup = BeautifulSoup(O2RawPage, 'html.parser')
-#    print soup.find_all("div", attrs={"id": "events_index"})
-    soup2 = soup.find_all("div", attrs={"class": "event_preview toLeft"})
-    print(soup2[0])
+List_events = []
 
+
+def cleanHTML(O2RawPage):
+    logging.info('cleanHTML function')
+    soup = BeautifulSoup(O2RawPage, 'html.parser')
+    soup2 = soup.find_all("div", attrs={"class": "event_preview toLeft"})
+
+    return soup2
+
+
+def insertInListEvents(Events):
+    logging.info('Inserting events in our list')
+    for i in range(len(Events)):
+        name = Events[i].find('h3').find('a').text
+        date_hour = Events[i].find('p').text
+        if date_hour.find(',') != -1:   # found!
+            date = Events[i].find('p').text.split(',')[0].replace(" ","")
+            hour = Events[i].find('p').text.split(',')[1][1:-4]
+        else:
+            date = date_hour
+            hour = "00:00"
+        infoLink = Events[i].find('h3').find('a').attrs['href']
+        ticketsLink = Events[i].find('a').attrs['href']
+        image = Events[i].find('div').attrs['style']
+       
+        event = {'name' : name,
+            'date' : date,
+            'hour' : hour,
+            'infoLink' : infoLink,
+            'ticketsLink' : ticketsLink,
+            'image': image}
+        # Insert event element in our list
+        List_events.append(event.copy())
 
 
 def curlURL(hola):
-    logging.info('curlURL function')
+    logging.info('curl: URL function')
     # we request to read an URL
     page = urllib2.urlopen(O2EventsURL)
-    # we read it and save it
-    page_content = page.read()
-    cleanHTML(page_content)
+    # we read it and send it
+    return page.read()
 
 
 def main():
-    curlURL(O2EventsURL)
+    rawEvents = cleanHTML(curlURL(O2EventsURL))
+    insertInListEvents(rawEvents)
+
 
 if __name__ == "__main__":
     sys.exit(main())
