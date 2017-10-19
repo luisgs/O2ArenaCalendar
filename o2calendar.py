a64a2cb485488748ce4122883a3108cec09f4953
Ubuntu <luisgs@MaquinaUbuntu.mistermapache.f9.internal.cloudapp.net>
2017-10-19 16:25:30 +0200

    polishing beautifulSoup

--- o2calendar.py
+++ o2calendar.py
@@ -1,4 +1,5 @@
-import sys, logging
+import sys
+import logging
 import urllib2
 from bs4 import BeautifulSoup
 
@@ -11,9 +12,16 @@ logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 List_events = []
 
+event = {'name': '',
+         'date': '',
+         'hour': "All day event",
+         'infoLink': "",
+         'ticketsLink': "",
+         'image': ""}
+
 
 def cleanHTML(O2RawPage):
-    logging.info('cleanHTML function')
+    logging.info('cleanHTML: extracting only HTML sectionwith events')
     soup = BeautifulSoup(O2RawPage, 'html.parser')
     soup2 = soup.find_all("div", attrs={"class": "event_preview toLeft"})
 
@@ -21,12 +29,14 @@ def cleanHTML(O2RawPage):
 
 
 def insertInListEvents(Events):
-    logging.info('Inserting events in our list')
+    # PRE: We received part of the HTML text, only the raw section we want
+    # SOL: we extract values we want AND insert them into our data structure
+    logging.info('InsertInListEvents: Inserting events in our list')
     for i in range(len(Events)):
         name = Events[i].find('h3').find('a').text
         date_hour = Events[i].find('p').text
         if date_hour.find(',') != -1:   # found!
-            date = Events[i].find('p').text.split(',')[0].replace(" ","")
+            date = Events[i].find('p').text.split(',')[0].replace(" ", "")
             hour = Events[i].find('p').text.split(',')[1][1:-4]
         else:
             date = date_hour
@@ -34,26 +44,31 @@ def insertInListEvents(Events):
         infoLink = Events[i].find('h3').find('a').attrs['href']
         ticketsLink = Events[i].find('a').attrs['href']
         image = Events[i].find('div').attrs['style']
-       
-        event = {'name' : name,
-            'date' : date,
-            'hour' : hour,
-            'infoLink' : infoLink,
-            'ticketsLink' : ticketsLink,
-            'image': image}
+
+        # We complete our event entity with our data
+        # data is stored in unicode. while printing will be fine
+        event = {'name': name.encode('utf8'),
+                 'date': date,
+                 'hour': hour,
+                 'infoLink': infoLink,
+                 'ticketsLink': ticketsLink,
+                 'image': image}
         # Insert event element in our list
         List_events.append(event.copy())
+    print(List_events[0]['name'])
+    print(List_events[0])
 
 
-def curlURL(hola):
-    logging.info('curl: URL function')
+def curlURL(URL):
+    logging.info('curl: URL function. Scraping website')
     # we request to read an URL
-    page = urllib2.urlopen(O2EventsURL)
+    page = urllib2.urlopen(URL)
     # we read it and send it
     return page.read()
 
 
 def main():
+    logging.info('MAIN: Main function')
     rawEvents = cleanHTML(curlURL(O2EventsURL))
     insertInListEvents(rawEvents)
 
